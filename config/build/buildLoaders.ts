59d9333ac3fd6e06f645d8bd441704f428e8053b
Danil <mydogfromhell666@gmail.com>
2022-10-12 00:15:43 +0300

    Add babel, add extract plugin for i18n

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,26 @@ export default function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	};
 
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+				'plugins': [
+					[
+						'i18next-extract',
+						{
+							locales: ['ru', 'en'],
+							keyAsDefaultValue: true
+						}
+					],
+				]
+			}
+		}
+	};
+
 	const fileLoader = {
 		test: /\.(png|jpe?g|gif)$/i,
 		use: [
@@ -45,6 +65,7 @@ export default function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
 	return [
 		fileLoader,
 		svgLoader,
+		babelLoader,
 		typescriptLoader,
 		cssLoader
 	];
